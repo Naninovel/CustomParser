d141d8d312832245df1e126816a44be3cfd07d2e
Artyom Sovetnikov <elringus@gmail.com>
2021-02-14 01:07:28 +0300

    update examples

--- Assets/Runtime/CustomGenericLineParser.cs
+++ Assets/Runtime/CustomGenericLineParser.cs
@@ -2,33 +2,30 @@
 using Naninovel.Commands;
 using Naninovel.Parsing;
 
-namespace Runtime
+public class CustomGenericLineParser : Naninovel.GenericTextLineParser
 {
-    public class CustomGenericLineParser : Naninovel.GenericTextLineParser
+    private float printSpeed;
+
+    protected override GenericTextScriptLine Parse (GenericTextLine lineModel)
     {
-        private float printSpeed;
+        // Reset current print speed.
+        printSpeed = -1;
 
-        protected override GenericTextScriptLine Parse (GenericTextLine lineModel)
+        // Try extract print speed from the model.
+        if (lineModel.AuthorIdentifier?.Text.Contains("-") ?? false)
         {
-            // Reset current print speed.
-            printSpeed = -1;
-
-            // Try extract print speed from the model.
-            if (lineModel.AuthorIdentifier?.Text.Contains("-") ?? false)
-            {
-                ParseUtils.TryInvariantFloat(lineModel.AuthorIdentifier.Text.GetAfter("-"), out printSpeed);
-                // Remove print speed from the author ID.
-                lineModel.AuthorIdentifier.Text = lineModel.AuthorIdentifier.Text.GetBeforeLast("-");
-            }
-            return base.Parse(lineModel);
+            ParseUtils.TryInvariantFloat(lineModel.AuthorIdentifier.Text.GetAfter("-"), out printSpeed);
+            // Remove print speed from the author ID.
+            lineModel.AuthorIdentifier.Text = lineModel.AuthorIdentifier.Text.GetBeforeLast("-");
         }
+        return base.Parse(lineModel);
+    }
 
-        protected override void AddCommand (Naninovel.Command command)
-        {
-            // When assigned, set the speed to all the print commands in the line.
-            if (printSpeed > 0 && command is PrintText print)
-                print.RevealSpeed = printSpeed / 100;
-            base.AddCommand(command);
-        }
+    protected override void AddCommand (Naninovel.Command command)
+    {
+        // When assigned, set the speed to all the print commands in the line.
+        if (printSpeed > 0 && command is PrintText print)
+            print.RevealSpeed = printSpeed / 100;
+        base.AddCommand(command);
     }
 }
